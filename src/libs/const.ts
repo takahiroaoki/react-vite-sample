3847a1185ff8e694a0901591ee25976175d5513f
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2025-04-26 03:36:12 +0000

    :recycle: refactor path & query use

--- src/libs/const.ts
+++ src/libs/const.ts
@@ -2,6 +2,9 @@ export class Path {
     static readonly TOP = '/'
     static readonly PAGE_A = '/pageA'
     static readonly PAGE_B = '/pageB'
+    
+    static readonly API_PREFIX = '/api'
+    static readonly API_EXAMPLE = this.API_PREFIX + '/example'
 }
 
 export class QueryParam {
