6e73f40417a4b19a4cb2415804a012798f402a92
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2025-05-09 00:59:16 +0000

    :recycle: change prettier config extension

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    printWidth: 150,
-    trailingComma: 'es5',
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    endOfLine: 'auto',
-};
--- src/organisms/selectForm/selectForm.tsx
+++ src/organisms/selectForm/selectForm.tsx
@@ -1,6 +1,6 @@
-import { Payload as ItemPayload } from "~/atoms/item/item";
-import { Group } from "~/libs/schemas";
-import AccordionList from "~/molecules/accordionList/accordionList";
+import { Payload as ItemPayload } from '~/atoms/item/item';
+import { Group } from '~/libs/schemas';
+import AccordionList from '~/molecules/accordionList/accordionList';
 
 export type Payload = {
   groups: Group[];
@@ -12,6 +12,7 @@ export default function SelectForm({ groups }: Props) {
   const onClickItemHandler = (itemPayload: ItemPayload) => {
     console.log(itemPayload.member.name);
   };
+  // TODO: add form component
   return (
     <>
       <AccordionList groups={groups} onClickItemFunc={onClickItemHandler} />
